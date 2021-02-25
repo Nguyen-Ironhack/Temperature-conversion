930f698941f4f4d56c49aeaf261e38e3cfc8d55d
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-25 16:08:53 +0100

    inheritence

--- src/components/BoilingVerdict.js
+++ src/components/BoilingVerdict.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { GreenLabel, GreenLabelWithIcon }  from './labels/GreenLabel';
+import { RedLabel } from './labels/RedLabel';
  
 const boilingVerdict = (props) => {
-    if (props.celsius >= 100) {
-        return <p>The water would boil.</p>;
-    } else {
-        return <p>The water would not boil.</p>;
-    }
+  if (props.celsius >= 200) {
+      return <GreenLabelWithIcon>It's over 200 Celsius, it's boooiiiilingg! </GreenLabelWithIcon>
+  } else if (props.celsius >= 100) {
+      return <GreenLabel>The water would boil.</GreenLabel>;
+  } else {
+      return <RedLabel>The water would not boil.</RedLabel>;
+  }
 }
  
 export default boilingVerdict;
